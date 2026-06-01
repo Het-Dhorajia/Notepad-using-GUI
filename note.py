b05ecd4d7f9820175ed6d3cd7419944fc4e87d5a
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-01 12:50:49 +0530

    pop up

--- note.py
+++ note.py
@@ -1,11 +1,10 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 
 root.geometry("800x600")
 root.title("Notepad")
-
-widget = Label(root, text="Hello", font=("Arial", 24))
 
 scroll = Scrollbar(root)
 scroll.pack(side=RIGHT, fill=Y)
@@ -15,6 +14,12 @@ text_box.pack(fill=BOTH, expand=True)
 
 scroll.config(command=text_box.yview)
 
+def exit_app():
+    root.destroy()
+
+def about_app():
+    messagebox.showinfo("About", "Notepad made by Het Dhorajia")
+
 menu_bar = Menu(root)
 
 file_menu = Menu(menu_bar, tearoff=0)
@@ -23,25 +28,19 @@ file_menu.add_command(label="New")
 file_menu.add_command(label="Open")
 file_menu.add_command(label="Save")
 file_menu.add_command(label="Save As")
-
 file_menu.add_separator()
-
-file_menu.add_command(label="Exit")
+file_menu.add_command(label="Exit", command=exit_app)
 
 menu_bar.add_cascade(label="File", menu=file_menu)
 
 edit_menu = Menu(menu_bar, tearoff=0)
-
 edit_menu.add_command(label="Cut")
 edit_menu.add_command(label="Copy")
 edit_menu.add_command(label="Paste")
-
 menu_bar.add_cascade(label="Edit", menu=edit_menu)
 
 help_menu = Menu(menu_bar, tearoff=0)
-
-help_menu.add_command(label="About")
-
+help_menu.add_command(label="About", command=about_app)
 menu_bar.add_cascade(label="Help", menu=help_menu)
 
 root.config(menu=menu_bar)
