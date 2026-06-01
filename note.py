ffeec43d9fb23ef64d7c21d6300089732006e27e
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-06-01 12:11:04 +0530

    text area & scroll bar done

--- note.py
+++ note.py
@@ -1,10 +1,16 @@
 from tkinter import *
-import os
-import tkinter as tk
 
-root =  Tk()
+root = Tk()
 
 root.geometry("800x600")
 root.title("Notepad")
+
+scroll = Scrollbar(root)
+scroll.pack(side=RIGHT, fill=Y)
+
+text_box = Text(root, yscrollcommand=scroll.set)
+text_box.pack(side=LEFT, fill=BOTH, expand=True)
+
+scroll.config(command=text_box.yview)
 
 root.mainloop()
